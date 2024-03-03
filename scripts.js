462e9e8bca805e225d7aaf0ba2f6b47e52f2fee4
Merveille94 <emanueleguehi@gmail.com>
2024-03-03 13:13:17 +0000

    update

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//Access DOM Element of the calculator
+const inputBox = document.getElementById('input');
+const expressionDiv = document.getElementById('expression')
+const resultDiv = document.getElementById('result')
+
+
