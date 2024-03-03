f2692ee1f633a138d9b6718f3f44ba01d76c8b0d
Merveille94 <emanueleguehi@gmail.com>
2024-03-03 14:01:58 +0000

    update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,48 @@
 //Access DOM Element of the calculator
-const inputBox = document.getElementById('input');
-const expressionDiv = document.getElementById('expression')
-const resultDiv = document.getElementById('result')
+const inputBox = document.getElementById("input");
+const expressionDiv = document.getElementById("expression");
+const resultDiv = document.getElementById("result");
 
+//define the expression and result variable
+let expression = "";
+let result = "";
 
+//define an event handler for the clicks
+function buttonClick(event) {
+  //Get values from clicked buttons
+  const target = event.target;
+  const action = target.dataset.action;
+  const value = target.dataset.value;
+  // console.log(target, action, value);
+
+  //Switch case to control the calculator
+  switch (action) {
+    case 'number':
+      addValue(value);
+      break;
+    case 'clear':
+      clear();
+      break;
+  }
+
+  //update the display
+  updateDisplay(expression, result);
+}
+
+inputBox.addEventListener("click", buttonClick);
+
+function addValue(value) {
+  //Add value to the expression
+  expression += value;
+  //   console.log(expression);
+}
+
+function updateDisplay(expression, result) {
+  expressionDiv.textContent = expression;
+  resultDiv.textContent = result;
+}
+
+function clear() {
+  expression = "";
+  result = " ";
+}
